f3849f4c22501176cbab545e8570e644d984096c
fmt <1105896230@qq.com>
2016-10-30 19:24:02 +0800

    不知道修改了什么东西

--- FixDemo/app/src/main/java/com/example/administrator/fixdemo/MainActivity.java
+++ FixDemo/app/src/main/java/com/example/administrator/fixdemo/MainActivity.java
@@ -1,39 +1,65 @@
 package com.example.administrator.fixdemo;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.view.ViewGroup;
+
+import com.lhh.ptrrv.library.PullToRefreshRecyclerView;
 
 public class MainActivity extends AppCompatActivity {
 
-
-    private TextView textView;
-
+    private static final int DEFAULT_ITEM_SIZE = 60;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        textView = (TextView) findViewById(R.id.text);
-        textView.setText("老版本");
-        oldPerson();
-
-    }
-
-    private void oldPerson() {
-        final Person person = new Person("小明", "男");
-        textView.setOnClickListener(new View.OnClickListener() {
+        PullToRefreshRecyclerView  mPtrrv = (PullToRefreshRecyclerView) this.findViewById(R.id.ptrrv);
+        mPtrrv.setSwipeEnable(true);//open swipe
+        mPtrrv.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false));
+        mPtrrv.setPagingableListener(new PullToRefreshRecyclerView.PagingableListener() {
             @Override
-            public void onClick(View v) {
-                Toast.makeText(MainActivity.this, person.getName(), Toast.LENGTH_SHORT).show();
+            public void onLoadMoreItems() {
             }
         });
-    }
+        mPtrrv.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+            }
+        });
+        PtrrvAdapter   mAdapter = new PtrrvAdapter(this);
+//        mAdapter.setCount(DEFAULT_ITEM_SIZE);
+        mPtrrv.setAdapter(mAdapter);
+        mPtrrv.onFinishLoading(true, false);
 
-    @Override
-    protected void onResume() {
-        super.onResume();
     }
+    private class PtrrvAdapter extends PtrrvBaseAdapter<PtrrvAdapter.ViewHolder> {
+        public PtrrvAdapter(Context context) {
+            super(context);
+        }
 
+
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = mInflater.inflate(R.layout.ptrrv_item, null);
+            return new ViewHolder(view);
+        }
+
+
+        @Override
+        public void onBindViewHolder(ViewHolder holder, int position) {
+
+        }
+
+        class ViewHolder extends RecyclerView.ViewHolder{
+
+            public ViewHolder(View itemView) {
+                super(itemView);
+            }
+        }
+    }
 }
